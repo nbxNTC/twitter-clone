0b628f14402914d8ea278a26803cff5234cd8d0c
Mario Sergio <marionbx.dev@gmail.com>
2022-03-01 15:32:42 -0300

    chore: configure roboto as default font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap' rel='stylesheet' />
+      </Head>
       <body>
         <Main />
         <NextScript />
