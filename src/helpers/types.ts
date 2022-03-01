95ba76b73aeb80b2b1fbba72e5f513ea115916fb
Mario Sergio <marionbx.dev@gmail.com>
2022-03-01 18:39:08 -0300

    feat: create post and user types

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,13 +1,35 @@
-export enum LogType {
+export enum LogTypeEnum {
   info = 'log',
   warning = 'warn',
   error = 'error',
 }
 
-export interface Log {
-  type: LogType
+export interface LogInterface {
+  type: LogTypeEnum
   componentName: string
   functionName: string
   message: string
   stackTrace?: any
+}
+
+export interface UserInterface {
+  photoUrl: string
+  name: string
+  nickname: string
+}
+
+export type PostType = 'POST' | 'REPOST'
+
+export interface PostInterface {
+  user: UserInterface
+  type: PostType
+  message?: string
+  post?: PostInterface
+  createdAt: Date
+}
+
+export interface MenuItemInterface {
+  icon?: React.ReactNode
+  name: string
+  onSubmit: () => void
 }
